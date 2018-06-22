c744e262f57c3bc5f80340216c135f640b68964f
cgcosta <37459859+cgcosta@users.noreply.github.com>
2018-06-22 20:51:57 -0300

    programa aula 22.06.2018

---
+++ verificacao.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class verificacao
+{
+private: 
+/*
+int carro;
+int vaga;
+int permissao
+*/
+public:
+
+int matricula;
+int enthora, entmin;
+int saihora, saimin;
+};
+
+//entrada
+
+//saida
+
+
+int main (){
+int n;
+if(n=!99)
+{
+cout << "Digite 0 para entrada ou 1 para saida" << endl;
+cin << n;
+
+if(n==0)
+
+//entrada()
+
+else
+
+//saida()
+}
+
+else
+break;
+
+
+
+verificacao v;
+
+cin >> v.matricula;
+cout << v.matricula << endl;
+
+cin >> v.enthora;
+cout << v.enthora << endl;
+
+cin >> v.entmin;
+cout << v.entmin << endl;
+
+
+
+return 0; 
+}
+
+
+//int carro = 1;
+//int vaga = 500
+
+
+/*
+Entrada:
+	Verifica lista de bloqueio – stop
+		Verifica lista de saída – lista de bloqueio – stop
+	Verifica lista de permissões – stop
+		Inclui na lista de saída
+	Atualiza n vagas
+Saída:
+	Verifica lista de bloqueio – stop
+		Verifica lista de entrada – lista de bloqueio – stop
+	Verifica lista de permissões – stop
+		Inclui na lista de saída
+	Atualiza n vagas
+*/
