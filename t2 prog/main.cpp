67d0a8ac8c7bceebc639e898d6eeeb76f99ba0d8
lbystronski <38798562+lbystronski@users.noreply.github.com>
2018-06-29 20:58:41 -0300

    Update main.cpp

--- t2 prog/main.cpp
+++ t2 prog/main.cpp
@@ -9,8 +9,8 @@ int main()
 {
     user a;
     int nvagas, opcao, verificador, mat, var;
-    float valor, x, y;
-
+    int valor, x, y;
+    a.saldo=0;
 
     cout << "Informe o numero de vagas: ";
     cin >> nvagas; cout << endl;
@@ -20,13 +20,13 @@ int main()
     {
         system("cls");
         cout << endl << "1 para entrada," << endl << "2 para saida," << endl << "3 para desbloquear," << endl << "4 para incluir saldo," << endl << "0 para finalizar: ";
-        cin >> opcao; // grava opÁ„o de entrada ou saida
+        cin >> opcao; // grava op√ß√£o de entrada ou saida
         if(opcao==0) break; if(opcao!=1 && opcao!=2 && opcao!=3 && opcao!=4){ cout << endl << "Opcao invalida" << endl;}
         if(opcao==1 || opcao==2 || opcao==3 || opcao==4)
         {
             mat=0; // zera matricula caso reinicie
             cout << endl << "Digite numero da matricula: ";
-            cin >> mat; // carrega matricula para variavel de verificaÁ„o de matricula
+            cin >> mat; // carrega matricula para variavel de verifica√ß√£o de matricula
 
             ifstream parkverifica;
             parkverifica.open("park.txt");
@@ -54,6 +54,7 @@ int main()
                             x=a.saldo-valor;
                             if (x<0) { cout << "Saldo insuficiente para sair" << endl; opcao==4; parkgrava << a.saldo << " ";}
                             if (x>=0 && a.estado!=0) {a.saldo=x; parkgrava << a.saldo << " ";}
+                            else{parkgrava << a.saldo << " ";}
                         }
                         if (opcao==4)
                         {
@@ -69,20 +70,22 @@ int main()
                         if (opcao==4){parkgrava << a.estado << endl;}
                         else{
 
-                        if (opcao==3 && a.estado==0) // se opÁao de desbloqueio
+                        if (opcao==3 && a.estado==0) // se op√ßao de desbloqueio
                         {
                             parkgrava << 3 << endl; // altera para estatus para desbloqueado
+                            var=5;
+
                         }
                         else
                         {
-                            if (opcao==1 && (a.estado==1 || a.estado==3)) // se opÁao de entrada e estatus liberado entrada ou desbloqueado
+                            if (opcao==1 && (a.estado==1 || a.estado==3)) // se op√ßao de entrada e estatus liberado entrada ou desbloqueado
                             {
                                 parkgrava << 2 << endl; // altera para estatus de entrada
                                 var=1;
                                 nvagas = nvagas - 1;
                             }
 
-                            if ((opcao==2 && (a.estado==2 || a.estado==3)) && x>=0) // se opÁao de saida e estatus liberado saida ou desbloqueado
+                            if ((opcao==2 && (a.estado==2 || a.estado==3)) && x>=0) // se op√ßao de saida e estatus liberado saida ou desbloqueado
                             {
                                 parkgrava << 1 << endl; // altera para estatus de saida
                                 var=2;
@@ -91,7 +94,7 @@ int main()
                             }
                             if ((opcao==2 && (a.estado==2 || a.estado==3)) && x<0){parkgrava << 2 << endl; var=3;y=x;}
 
-                            if (((opcao==1 && a.estado==2) || (opcao==2 && a.estado==1) || (a.estado==0)) && opcao!=4) // se opÁao de entrada e estatus n„o correspondem
+                            if (((opcao==1 && a.estado==2) || (opcao==2 && a.estado==1) || (a.estado==0)) && opcao!=4) // se op√ßao de entrada e estatus n√£o correspondem
                             {
                                 parkgrava << 0 << endl; // altera para estatus para bloqueado
                                 var=0;
@@ -123,7 +126,7 @@ int main()
                 parkverifica2 << a.estado << endl;
 
             }parkverifica2.close(); parkgrava2.close();
-            remove("park2.txt");
+            //remove("park2.txt");
 
         }
 
@@ -133,6 +136,7 @@ int main()
         if(var==1){cout << endl<< endl << "Entrada liberada com sucesso:" << endl; cout << "Saldo: "<< x << endl; cout << "Numero de vagas:" << nvagas << endl<< endl;}
         if(var==2){cout << endl<< endl << "Saida liberada com sucesso:" << endl; cout << "Saldo: "<< x << endl; cout << "Numero de vagas:" << nvagas << endl<< endl;}
         if(var==3){cout << endl<< endl << "Saida nao permitida:" << endl<< endl;}
+        if(var==5){cout << endl<< endl << "Desbloqueado com sucesso:" << endl<< endl;}
         system("pause");
         //system("cls");
     }
